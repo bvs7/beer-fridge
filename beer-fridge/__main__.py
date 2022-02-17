6eb763e034df522ec6d0bbcf0f49d2cdaadec77b
Brian Scaramella <bvs7@cornell.edu>
2022-02-17 16:02:52 -0700

    Link Controller and server

--- beer-fridge/__main__.py
+++ beer-fridge/__main__.py
@@ -1,14 +1,118 @@
-from flask import Flask, request
+import logging
+import threading
 
-from controller import FridgeController
+from flask import Flask, request, abort, jsonify
+
+from . import FridgeController, CtoF
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+TEMP_ID = 'temp'
+F_ID = 'F'
+C_ID = 'C'
+
+UPPER_LIMIT_ID = 'upper_limit'
+LOWER_LIMIT_ID = 'lower_limit'
+DELAY_ID = 'delay'
+FRIDGE_ON_ID = 'fridge_on'
+
+def check_for_temp(d):
+    if TEMP_ID in d:
+        return d[TEMP_ID]
+    if F_ID in d:
+        return d[F_ID]
+    if C_ID in d:
+        logging.debug(d[C_ID])
+        return CtoF( float(d[C_ID]) )
+    return None
 
 fridgeController = FridgeController()
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    result = True
+    if request.method == 'GET':
+        if UPPER_LIMIT_ID in request.args:
+            result = result and fridgeController.setUpperLimit(request.args[UPPER_LIMIT_ID])
+        if LOWER_LIMIT_ID in request.args:
+            result = result and fridgeController.setLowerLimit(request.args[LOWER_LIMIT_ID])
+        if DELAY_ID in request.args:
+            result = result and fridgeController.setDelay(request.args[DELAY_ID])
+
+        if not result:
+            abort(400)
+        
+        d = {
+            UPPER_LIMIT_ID:fridgeController.upper_limit,
+            LOWER_LIMIT_ID:fridgeController.lower_limit,
+            FRIDGE_ON_ID:fridgeController.fridge_on,
+            DELAY_ID:fridgeController.delay,
+            TEMP_ID:fridgeController.tempF
+        }
+
+        return jsonify(d)
+
+@app.route(f'/{UPPER_LIMIT_ID}', methods=['GET','POST'])
+def upper_limit():
+    if request.method == 'POST':
+        temp = check_for_temp(request.form)
+        if not temp:
+            abort(400, "No temperature limit ('F', 'C') supplied")
+        
+    elif request.method == 'GET':
+        temp = check_for_temp(request.args)
+        if not temp:
+            return str(fridgeController.upper_limit)
+
+    if fridgeController.setUpperLimit(temp):
+        return "Success"
+    else:
+        abort(400, f"Invalid temperature, {temp}")
+
+@app.route(f'/{LOWER_LIMIT_ID}', methods=['GET','POST'])
+def lower_limit():
+    if request.method == 'POST':
+        temp = check_for_temp(request.form)
+        if not temp:
+            abort(400, "No temperature limit ('F', 'C') supplied")
+        
+    elif request.method == 'GET':
+        temp = check_for_temp(request.args)
+        if not temp:
+            return str(fridgeController.lower_limit)
+
+    if fridgeController.setLowerLimit(temp):
+        return "Success"
+    else:
+        abort(400, f"Invalid temperature, '{temp}'")
+
+@app.route(f'/{FRIDGE_ON_ID}', methods=['GET'])
+def getFridge_on():
+    return str(fridgeController.fridge_on)
+
+@app.route(f'/{DELAY_ID}',methods=['GET','POST'])
+def delay():
+    return str(fridgeController.delay)
+        
+@app.route(f'/{TEMP_ID}')
+def temp():
+    return str(fridgeController.tempF)
 
 if __name__ == '__main__':
+
+    ctrl = threading.Thread(target = fridgeController.run, name = "fridgeController")
+
+    ctrl.start()
+    logging.info("Controller Started")
+
     app.run(debug=True)
+
+    logging.info("Exiting")
+
+    fridgeController.quit = True
+    ctrl.join()
+
+    logging.info("Controller Stopped")
--- beer-fridge/controller.py
+++ beer-fridge/controller.py
@@ -20,10 +20,13 @@ except ModuleNotFoundError:
 
 __all__ = ['FridgeController']
 
-DEFAULT_UPPER_LIMIT = 54
-DEFAULT_LOWER_LIMIT = 50
+DEFAULT_UPPER_LIMIT = 54.0
+DEFAULT_LOWER_LIMIT = 50.0
 DEFAULT_RELAY_PIN = 4
-DEFAULT_DELAY_S = 5.0 # S
+DEFAULT_DELAY_S = 60.0 # S
+
+LIMIT_LOWER_LIMIT = -20
+LIMIT_UPPER_LIMIT = 100
 
 def CtoF(tempC):
     return tempC * 9.0/5.0 + 32.0
@@ -49,26 +52,67 @@ class FridgeController:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_pin,GPIO.OUT)
 
-        self.fridge_on = GPIO.input(self.relay_pin)
+        self.fridge_on = bool(GPIO.input(self.relay_pin))
 
     def run(self):
+        logging.info("Controller Running")
         while not self.quit:
-            tempC = self.temp_sensor.readTempC()
-            tempF = CtoF(tempC)
+            self.tempC = self.temp_sensor.readTempC()
+            self.tempF = CtoF(self.tempC)
             logging.debug(str(datetime.datetime.now()))
-            logging.info("Temp: {}, Fridge On?: {}".format(tempF, self.fridge_on))
+            logging.info("Temp: {}, Fridge On?: {}".format(self.tempF, self.fridge_on))
 
             if not self.fridge_on:
-                if tempF > self.upper_limit:
+                if self.tempF > self.upper_limit:
                     if __name__ == "__main__":
                         logging.info("FRIDGE ON")
                     self.fridge_on = True
                     GPIO.output(self.relay_pin, GPIO.HIGH)
             else:
-                if tempF < self.lower_limit:
+                if self.tempF < self.lower_limit:
                     if __name__ == "__main__":
                         logging.info("FRIDGE OFF")
                     self.fridge_on = False
                     GPIO.output(self.relay_pin, GPIO.LOW)
 
-            time.sleep(self.delay)
+            n = 0
+            while True:
+                if self.quit or n >= self.delay:
+                    break
+                n += 1
+                time.sleep(1)
+
+    def setUpperLimit(self, limit):
+        try:
+            limit = float(limit)
+        except ValueError:
+            return False
+        
+        if limit < LIMIT_LOWER_LIMIT or limit > LIMIT_UPPER_LIMIT:
+            return False
+
+        self.upper_limit = limit
+        return True
+    
+    def setLowerLimit(self, limit):
+        try:
+            limit = float(limit)
+        except ValueError:
+            return False
+        
+        if limit < LIMIT_LOWER_LIMIT or limit > LIMIT_UPPER_LIMIT:
+            return False
+
+        self.lower_limit = limit
+        return True
+
+    def setDelay(self, delay):
+        try:
+            delay = float(delay)
+        except ValueError:
+            return False
+        if delay < 1.0 or delay > 60*60*4: #longer than 1 min, shorter than 4 hours
+            return False
+
+        self.delay = delay
+        return True
